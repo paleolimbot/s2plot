8702656b33164df86b5d5476c6dae473ea7541d6
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-24 20:35:43 -0300

    projection skeleton

---
+++ R/proj.R
@@ -0,0 +1,50 @@
+
+#' Define a projection to use with s2plot
+#'
+#' @param point The centre of the projection
+#' @param rotation Rotation
+#' @param projection Right now [s2plot_projection_orthographic()]
+#'   is the only projection.
+#' @inheritParams s2plot
+#'
+#' @export
+#'
+s2plot_projection_orthographic <- function(point, rotation = 0) {
+  structure(
+    list(point = c(libs2::s2_x(point), libs2::s2_y(point)), rotation = rotation),
+    class = "s2plot_projection_orthographic"
+  )
+}
+
+#' @rdname s2plot_projection_orthographic
+#' @export
+s2plot_prepare <- function(projection, geog) {
+  UseMethod("s2plot_prepare")
+}
+
+#' @rdname s2plot_projection_orthographic
+#' @export
+s2plot_project <- function(projection, geog) {
+  UseMethod("s2plot_project")
+}
+
+#' @rdname s2plot_projection_orthographic
+#' @export
+s2plot_prepare.s2plot_projection_orthographic <- function(projection, geog) {
+  hemisphere <- libs2::s2geography(make_hemisphere_wkt(projection$point[1], projection$point[2]))
+  libs2::s2_intersection(geog, hemisphere)
+}
+
+#' @rdname s2plot_projection_orthographic
+#' @export
+s2plot_project.s2plot_projection_orthographic <- function(projection, geog) {
+  xy <- wk::wkb_coords(libs2::s2_asbinary(geog), sep_na = TRUE)
+
+  projected <- mapproj::mapproject(
+    xy$x, xy$y,
+    projection = "orthographic",
+    orientation = c(projection$point, projection$rotation)
+  )
+
+  projected[c("x", "y")]
+}
---
+++ R/s2plot.R
@@ -0,0 +1,26 @@
+
+#' Plot an object on the sphere
+#'
+#' @param geog A [libs2::s2geography()]
+#' @param xlim,ylim Limits in projected space (usually in the range -1 to 1).
+#' @param ... Passed to graphics functions.
+#'
+#' @return `x`, invisibly
+#' @export
+#'
+s2plot <- function(geog, ..., xlim = NULL, ylim = NULL) {
+
+}
+
+#' @rdname s2plot
+#' @export
+s2plot_new <- function(geog, ..., xlim = NULL, ylim = NULL) {
+
+
+}
+
+#' @rdname s2plot
+#' @export
+s2plot_draw <- function(geog, ...) {
+
+}
