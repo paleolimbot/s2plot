b3a4cfb1ea152a2a71e859f496a88db2aa2fd9a2
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-25 09:09:38 -0300

    also test line

--- tests/testthat/test-s2plot.R
+++ tests/testthat/test-s2plot.R
@@ -2,6 +2,7 @@
 test_that("basic plotting works", {
   s2plot(libs2::s2data_countries(), col = "grey90")
   s2plot(libs2::s2data_cities("London"), pch = 16, add = T)
+  s2plot("LINESTRING (0 0, 0 45)", add = T)
 
   expect_true(TRUE)
 })
@@ -9,6 +10,7 @@ test_that("basic plotting works", {
 test_that("plotting with manual projection works", {
   s2plot(libs2::s2data_countries(), projection = s2plot_projection_orthographic("POINT (0 0)"))
   s2plot(libs2::s2data_cities("London"), pch = 16, add = T)
+  s2plot("LINESTRING (0 0, 0 45)", add = T)
 
   expect_true(TRUE)
 })
